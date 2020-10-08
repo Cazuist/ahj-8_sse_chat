37f3207b81c41f858af17d350f337e97fcf81377
Cazuist <sovrus@mail.ru>
2020-10-08 09:21:39 +0300

    Add wss protocol

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 import ChatWidget from './ChatWidget';
 
 //const chat = new ChatWidget(document.body, 'ws://localhost:7070');
-const chat = new ChatWidget(document.body, 'ws://ahj89ws.herokuapp.com');
+const chat = new ChatWidget(document.body, 'wss://ahj89ws.herokuapp.com');
 
 chat.init();
